af9b889c717dc5eb001ffee6940e6d99b83de5a8
Plyushchenko <plyushenko@bk.ru>
2018-05-09 13:41:30 +0300

    removed comment

--- 001-cli/src/main/java/ru/spbau/cli/Main.java
+++ 001-cli/src/main/java/ru/spbau/cli/Main.java
@@ -2,13 +2,6 @@ package ru.spbau.cli;
 
 import java.io.IOException;
 
-/**
- * Пофиксить "abc' 10:21-10:31
- * Написать документацию 10:32-10:50
- * Реализовать grep 10:52-11:58
- * Написать тесты 30 мин 12:14
- * Написать, почему выбрал JCommander 30 мин
- */
 public class Main {
     public static void main(String[] args) {
         try {
