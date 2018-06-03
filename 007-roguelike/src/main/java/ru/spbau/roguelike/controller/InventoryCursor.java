eaa78400ef87d0c90e5efd29863d5b0d3e5e8d4c
Plyushchenko <plyushenko@bk.ru>
2018-06-03 12:49:41 +0300

    fixed tiny bug, introduced logger

--- 007-roguelike/src/main/java/ru/spbau/roguelike/controller/InventoryCursor.java
+++ 007-roguelike/src/main/java/ru/spbau/roguelike/controller/InventoryCursor.java
@@ -1,5 +1,7 @@
 package ru.spbau.roguelike.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import ru.spbau.roguelike.model.modifier.artifact.Artifact;
 import ru.spbau.roguelike.model.modifier.artifact.Inventory;
 import ru.spbau.roguelike.model.modifier.artifact.Slot;
@@ -8,6 +10,7 @@ import ru.spbau.roguelike.model.modifier.artifact.Slot;
  * GUI Inventory handler
  */
 public class InventoryCursor {
+    private static final Log LOGGER = LogFactory.getLog(InventoryCursor.class);
     private static int INACTIVE_ARTIFACTS_BLOCK = 0;
     private static int ACTIVE_ARTIFACTS_BLOCK = 1;
     private int block;
@@ -32,6 +35,9 @@ public class InventoryCursor {
      * Get the next element of a list or go to the beginning of the next list (cycled)
      */
     void increment() {
+        if (LOGGER != null) {
+            LOGGER.info("Before increment: block = " + block + ", number = " + number);
+        }
         if (block == INACTIVE_ARTIFACTS_BLOCK) {
             if (number == inventory.getActiveArtifacts().size() - 1) {
                 if (inventory.getInactiveArtifacts().size() != 0) {
@@ -49,12 +55,18 @@ public class InventoryCursor {
                 number++;
             }
         }
+        if (LOGGER != null) {
+            LOGGER.info("After increment: block = " + block + ", number = " + number);
+        }
     }
 
     /**
      * Get the previous element of a list or go to the bottom of the previous list (cycled)
      */
     void decrement() {
+        if (LOGGER != null) {
+            LOGGER.info("Before decrement: block = " + block + ", number = " + number);
+        }
         if (block == INACTIVE_ARTIFACTS_BLOCK) {
             if (number == 0) {
                 if (inventory.getInactiveArtifacts().size() != 0) {
@@ -68,11 +80,14 @@ public class InventoryCursor {
             }
         } else if (block == ACTIVE_ARTIFACTS_BLOCK) {
             if (number == 0) {
-                block = ACTIVE_ARTIFACTS_BLOCK;
+                block = INACTIVE_ARTIFACTS_BLOCK;
                 number = inventory.getActiveArtifacts().size() - 1;
             } else {
                 number--;
             }
+        }
+        if (LOGGER != null) {
+            LOGGER.info("After decrement: block = " + block + ", number = " + number);
         }
     }
 
@@ -80,6 +95,9 @@ public class InventoryCursor {
      * Put on or take off an artifact at the cursor position
      */
     void useArtifact() {
+        if (LOGGER != null) {
+            LOGGER.info("At useArtifact: block = " + block + ", number = " + number);
+        }
         if (block == INACTIVE_ARTIFACTS_BLOCK) {
             int i = 0;
             for (Slot slot: inventory.getActiveArtifacts().keySet()) {
@@ -95,17 +113,17 @@ public class InventoryCursor {
                 if (i == number) {
                     inventory.putOn(artifact);
                     block = INACTIVE_ARTIFACTS_BLOCK;
-                    number = 0;
+                    int tmpNumber = 0;
                     for (Slot slot: inventory.getActiveArtifacts().keySet()) {
                         if (artifact.getSlot() == slot) {
+                            number = tmpNumber;
                             return;
                         }
-                        number++;
+                        tmpNumber++;
                     }
                 }
                 i++;
             }
-
         }
     }
 }
